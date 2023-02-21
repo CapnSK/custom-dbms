17a5c7a475968d0ad43b9ccc2ef42d1627f9f4fc
Sankalp Kulkarni <sn573012@dal.ca>
2023-02-21 18:22:08 -0400

    created user registration flow and added service methods for persistence

--- Service/PersistenceService.java
+++ Service/PersistenceService.java
@@ -1,5 +1,106 @@
 package Service;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import Model.User;
+
 public class PersistenceService {
-    
+    private static List<User> usersCache = null;
+
+    private PersistenceService(){
+
+    }
+
+    private static String delimiter = "_$_";
+    private static String userStore = "/data/users.txt";
+
+
+    public static Boolean storeUserCredentials(User user){
+        Boolean userStored = true;
+        if(user != null && user.getName() != null 
+            && user.getEncryptedPassword() != null 
+            && user.getSecurityQuestionAnswer() != null
+        ){
+            File file = new File(System.getProperty("user.dir") + userStore);
+            if(!file.exists()){
+                try {
+                    file.getParentFile().mkdirs();
+                    userStored = file.createNewFile();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    userStored = false;
+                }
+            }
+            if(file.isFile() && file.exists()){
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(file,true))) {
+                    writer.append(user.getName());
+                    writer.append(delimiter);
+                    writer.append(user.getUsername());
+                    writer.append(delimiter);
+                    writer.append(user.getEncryptedPassword());
+                    writer.append(delimiter);
+                    writer.append(user.getSecurityQuestion());
+                    writer.append(delimiter);
+                    writer.append(user.getSecurityQuestionAnswer());
+                    writer.append("\n");
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    userStored = false;
+                }
+            }
+            else{
+                userStored = false;
+            }
+        }
+        return userStored;
+    }
+
+    public static List<User> getAllUsers(){
+        return getAllUsers(false);
+    }
+
+    public static List<User> getAllUsers(Boolean forceUpdate){
+        if(forceUpdate || (usersCache == null || !usersCache.isEmpty())){
+
+            if(usersCache == null){
+                usersCache = new ArrayList<>();
+            } else {
+                usersCache.clear();
+            }
+
+            try (Scanner fileReader = new Scanner(new File(System.getProperty("user.dir") + userStore))) {
+                while(fileReader.hasNextLine()){
+                    String input = fileReader.nextLine();
+                    if(!input.equals("\n")){
+                        List<String> data = Arrays.asList(input.split(delimiter.replace("$", "\\$"), -1));
+                        if(data.size() == 5){
+                            User user = new User(
+                                data.get(0), 
+                                data.get(1), 
+                                null, 
+                                data.get(3), 
+                                data.get(4)
+                            );
+                            user.setEncryptedPassword(data.get(2));
+                            usersCache.add(user);
+                        }
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return usersCache;
+    }
 }
--- Service/UserRegistrationService.java
+++ Service/UserRegistrationService.java
@@ -1,7 +1,78 @@
 package Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import Model.User;
+
 public class UserRegistrationService {
+    private static String encryptionAlgorithm = "MD5";
+
+    private static Scanner input = new Scanner(System.in);
     public static Boolean registerUser(){
-        return false;
+        User user = UserRegistrationService.createUserModel();
+        try {
+            user.setEncryptedPassword(UserRegistrationService.encryptPassword(user.getPassword()));
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return PersistenceService.storeUserCredentials(user);
+    }
+
+    private static User createUserModel(){
+        System.out.println("Please enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Please enter preferred username (should be unique): ");
+        String userName = null;
+        Boolean userNameTaken = true;
+        do{
+            userName = input.nextLine();
+            userNameTaken = UserRegistrationService.checkIfUserExists(userName);
+            if(userNameTaken){
+                System.out.println("This user name is taken, please enter a different username: ");
+            }
+        } while(userNameTaken);
+
+        System.out.println("Please enter your password: ");
+        String password = input.nextLine();
+        System.out.println("Re enter your password: ");
+        String passwordCheck = input.nextLine();
+
+        while(!password.equals(passwordCheck)){
+            System.out.println("Both password do not match, please try again");
+            passwordCheck = input.nextLine();
+        }
+
+        System.out.println("Provide security question: ");
+        String securityQuestion = input.nextLine();
+        System.out.println("Provide answer to the question: ");
+        String securityQuestionAnswer = input.nextLine();
+
+        return new User(name, userName, password, securityQuestion, securityQuestionAnswer);
+    }
+
+
+    private static Boolean checkIfUserExists(String username){
+        Boolean userExists = false;
+        List<User> users = PersistenceService.getAllUsers();
+        if(users != null && username != null){
+            userExists = users.stream().anyMatch(user ->{
+                return username.equals(user.getUsername());
+            });
+        }
+        return userExists;
+    }
+
+    private static String encryptPassword(String password) throws NoSuchAlgorithmException{
+        String encryptedPassword = password;
+        MessageDigest MD5 = MessageDigest.getInstance(encryptionAlgorithm);
+        MD5.update(encryptedPassword.getBytes(), 0, 0);
+        encryptedPassword = new String(MD5.digest(), StandardCharsets.UTF_8);
+        return encryptedPassword;
     }
 }
